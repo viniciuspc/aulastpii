27841f921b546d90fc072fded005b30984563042
pasqualini9@gmail.com <pasqualini9@gmail.com@4e4403fb-5103-f92f-b5a6-a14907b5738a>
2011-08-25 13:53:11 +0000

    exercicios 2 e 3 Exibir uma nova  questão Contabilização do score feita corretamente

--- trunk/AulaJSF001B/src/java/br/com/fiap/beans/QuizzSimples.java
+++ trunk/AulaJSF001B/src/java/br/com/fiap/beans/QuizzSimples.java
@@ -13,12 +13,15 @@ import javax.faces.bean.SessionScoped;
  *
  * @author eluquini
  */
+
+//backbean -> bean associado com a interface
 @ManagedBean(name="quizz")
 @SessionScoped
 public class QuizzSimples {
 
     private ArrayList<Problema> problemas = new ArrayList<Problema>();
     private int score = 0;
+    private int cont = -1;
 
     public int getScore() {
         return score;
@@ -30,14 +33,22 @@ public class QuizzSimples {
     }
 
     public String getSequencia(){
-        return problemas.get(0).getSequencia();
+        cont++;
+        if(problemas.size() <= cont)
+            cont=0;
+
+        return problemas.get(cont).getSequencia();
     }
 
-    public String getResposta(){
+    public String getResposta(){  //getResposta é nescessário para o JSF poi quando carrega a pagina ele tenta setar o campo de texto com a propriedade
         return "";
     }
 
+    
+
     public void setResposta(String resposta){
-        score++;
+        if(!resposta.equals(""))
+            if(problemas.get(cont).getSolução() == Integer.parseInt(resposta))
+                score++;
     }
 }
