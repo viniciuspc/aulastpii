7c0d8133aa36847513e4d2138a830dcc0e134a67
pasqualini9@gmail.com <pasqualini9@gmail.com@4e4403fb-5103-f92f-b5a6-a14907b5738a>
2011-09-01 11:59:50 +0000

    MeuBackBean. java adicionado

---
+++ trunk/ExemploSelectJSF/src/java/br/com/fiap/exemplos/MeuBackBean.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.fiap.exemplos;
+
+import java.util.ArrayList;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author rm61828
+ */
+@ManagedBean(name="bean")
+@SessionScoped
+public class MeuBackBean {
+
+    public MeuBackBean(){
+        opcoes = new ArrayList<String>();
+        opcoes.add("viagem");
+        opcoes.add("futebol");
+        opcoes.add("videogame");
+        opcoes.add("namorar");
+        opcoes.add("cerveja");
+    }
+
+    private String escolha;
+    private ArrayList<String> opcoes;
+
+    public String getEscolha() {
+        return escolha;
+    }
+
+    public void setEscolha(String escolha) {
+        this.escolha = escolha;
+    }
+
+    public ArrayList<String> getOpcoes() {
+        return opcoes;
+    }
+
+    public void setOpcoes(ArrayList<String> opcoes) {
+        this.opcoes = opcoes;
+    }
+}
